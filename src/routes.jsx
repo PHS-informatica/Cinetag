5703c39bf8d588775e85eea82d7494f876953c20
phs-informatica <paulohsantos2008@hotmail.com>
2025-01-07 10:47:57 -0300

    2 commit

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './Pages/Inicio/Index.jsx'
-import Favoritos from './Pages/Favoritos/Index.jsx';
-import Player from './Pages/Player/index';
+import Favoritos from './Pages/Favoritos/index.jsx';
+import Player from './Pages/Player/index.jsx';
 import NaoEncontrada from './Pages/NaoEncontrada/index.jsx';
 import PaginaBase from './Pages/PaginaBase/Index.jsx';
 
