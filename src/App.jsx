1988a19e7bde8556163abaf11a24d0a419597be3
phs-informatica <paulohsantos2008@hotmail.com>
2025-01-07 11:03:59 -0300

    4 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
+
 import './App.css'
 import AppRoutes from './routes.jsx'
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <AppRoutes />
   )
